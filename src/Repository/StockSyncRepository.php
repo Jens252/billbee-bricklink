45acc591cd36f8c39b493f1c0fb524f01122381d
Jens Borgemeister <jens1596@gmail.com>
2025-05-08 15:16:49 +0200

    do not sync parts from Billbee to BrickLink

--- src/Repository/StockSyncRepository.php
+++ src/Repository/StockSyncRepository.php
@@ -34,27 +34,30 @@ class StockSyncRepository implements StockSyncRepositoryInterface
             // Fetch current inventory data from the Bricklink API
             $product = ResponseHelper::getData($this->client->get("inventories/$productId"));
 
-            // Check that quantity is not negative
-            $quantity = max(0, $quantity);
+            // Do not sync parts (this would ignore partout on BrickLink)
+            if ($product['item']['type'] != 'PART') {
+                // Check that quantity is not negative
+                $quantity = max(0, $quantity);
 
-            // If a max quantity is set for sets, ensure the quantity doesn't exceed the limit
-            if (isset($this->maxQuantityForSets) and $product['item']['type'] == 'SET') {
-                $quantity = min($this->maxQuantityForSets, $quantity);
+                // If a max quantity is set for sets, ensure the quantity doesn't exceed the limit
+                if (isset($this->maxQuantityForSets) and $product['item']['type'] == 'SET') {
+                    $quantity = min($this->maxQuantityForSets, $quantity);
+                }
+
+                // Prepare parameters for the stock update request
+                $parameters = ['quantity' => $quantity - $product['quantity'], 'is_retain' => true];
+
+                // Handle scenarios where stock needs to be moved to or out of the stockroom
+                if ($quantity == 0) {
+                    $parameters['is_stock_room'] = true;
+                    $parameters['stock_room_id'] = $this->multiStockroom ? 'C' : 'S';
+                } elseif ($quantity > 0 & $product['quantity'] == 0) {
+                    $parameters['is_stock_room'] = false;
+                }
+
+                // Send the stock update request
+                $this->client->put("inventories/$productId", ['json' => $parameters]);
             }
-
-            // Prepare parameters for the stock update request
-            $parameters = ['quantity' => $quantity - $product['quantity'], 'is_retain' => true];
-
-            // Handle scenarios where stock needs to be moved to or out of the stockroom
-            if ($quantity == 0) {
-                $parameters['is_stock_room'] = true;
-                $parameters['stock_room_id'] = $this->multiStockroom ? 'C' : 'S';
-            } elseif ($quantity > 0 & $product['quantity'] == 0) {
-                $parameters['is_stock_room'] = false;
-            }
-
-            // Send the stock update request
-            $this->client->put("inventories/$productId", ['json' => $parameters]);
         } catch (GuzzleException $e) {
             // Log the error and throw a ProductNotFoundException if the update fails
             $this->logger->error("Stock update failed for $productId:" . $e->getMessage());
